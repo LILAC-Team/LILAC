6b63e5cfc32f1c43a02815140a890d33879efc26
yunji9151 <yunji9151@naver.com>
2023-04-24 15:19:43 +0900

    ✨ Feat: BasicImage Components 추가
    1. BasicImage Components 추가
    2. test page 수정

--- frontend/pages/test.tsx
+++ frontend/pages/test.tsx
@@ -1,8 +1,17 @@
+import { BasicImage } from "@/components/common/BasicImage/style";
 import BasicText from "@/components/common/BasicText";
 import React from "react";
 
 const Test = () => {
-  return <BasicText text="테스트" color="#ffffff" font="" size={16} />;
+  return (
+    <>
+      <BasicText text="테스트" color="#ffffff" font="" size={16} />
+      <BasicImage
+        src="https://i.namu.wiki/i/m60BZ35BZRrbiqpurjIPB7GAs74I2LPNXe0MuHeEemha3ksZzGJo21PfgIdXn6JXZV0Wnps6xiAMPCVb_BYIwMeDwGEtL1R9Sxe5lmGUb4ZlPMyUO-vxTNG-6RMTR23h-myh5DqQk0h38DUi-wxiUA.jpg"
+        size={20}
+      />
+    </>
+  );
 };
 
 export default Test;
