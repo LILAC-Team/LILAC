844a8d19678b6d577ead8f39f242cc3ed6038b08
김현수 <gustn913@naevr.com>
2023-04-24 15:01:20 +0900

    ✨ Feat: BasicSlider Component 추가
    BasicSlider Component 추가

--- frontend/components/Home/BasicSlider/index.tsx
+++ frontend/components/Home/BasicSlider/index.tsx
@@ -1,5 +1,63 @@
-interface BasicSliderProps {}
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import * as S from "./style";
 
-export const BasicSlider = () => {
-  return <div>index</div>;
+const dummy: { title: string }[] = [
+  {
+    title: "slide 1",
+  },
+  {
+    title: "slide 2",
+  },
+  {
+    title: "slide 3",
+  },
+  {
+    title: "slide 4",
+  },
+  {
+    title: "slide 5",
+  },
+  {
+    title: "slide 6",
+  },
+  {
+    title: "slide 7",
+  },
+  {
+    title: "slide 8",
+  },
+  {
+    title: "slide 9",
+  },
+];
+
+interface BasicSliderProps {
+  imageSize?: string;
+  imageText?: string;
+  fetchUrl?: string;
+  handleSetShowModal?: Function;
+}
+// import required modules
+const BasicSlider = ({
+  imageSize,
+  imageText,
+  fetchUrl,
+  handleSetShowModal,
+}: BasicSliderProps) => {
+  return (
+    <>
+      <Swiper slidesPerView={3} spaceBetween={30}>
+        {dummy.map((value: { title: string }) => (
+          <SwiperSlide key={value.title}>
+            <S.DummyWrap>
+              <S.ContentWrap>{value.title}</S.ContentWrap>
+            </S.DummyWrap>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
 };
+
+export default BasicSlider;
