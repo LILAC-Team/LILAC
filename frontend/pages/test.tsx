ab674e330893c97897a2ce1105d36948bdeb53a9
yunji9151 <yunji9151@naver.com>
2023-04-24 14:32:55 +0900

    📝 Rename: branch 수정
    1. front에서 frontend로 branch 이름 수정
    2. test 파일 수정

--- frontend/pages/test.tsx
+++ frontend/pages/test.tsx
@@ -2,7 +2,7 @@ import BasicText from "@/components/common/BasicText";
 import React from "react";
 
 const Test = () => {
-  return <BasicText text="테스트" color="#ffffff" font="" size={32} />;
+  return <BasicText text="테스트" color="#ffffff" font="" size={16} />;
 };
 
 export default Test;
