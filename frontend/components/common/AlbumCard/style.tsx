06233d85925eda8b8f071e9b4937e840e965e8a4
yunji9151 <yunji9151@naver.com>
2023-04-25 10:47:13 +0900

    ✨ Feat: AlbumCard Components 추가
    1. AlbumCard Component 추가
    2. test(json 포함) 코드 추가

---
+++ frontend/components/common/AlbumCard/style.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const AlbumCard = styled.div`
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding: 1rem;
+  cursor: pointer;
+  &:active {
+    color: white;
+  }
+`;
+
+export const AlbumCardImg = styled.div``;
+export const AlbumCardTitle = styled.div`
+  margin: 0.5rem;
+`;
+export const AlbumCardDetail = styled.div``;
