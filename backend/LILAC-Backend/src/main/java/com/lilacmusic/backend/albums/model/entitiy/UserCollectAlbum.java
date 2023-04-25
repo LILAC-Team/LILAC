ac6f28a88746ad18731f683a5e55f8b27c5c6e1b
김명준 <audwnswmf@naver.com>
2023-04-25 09:26:41 +0900

    🐛 Fix: JPA 테이블에 코멘트 추가

--- backend/LILAC-Backend/src/main/java/com/lilacmusic/backend/albums/model/entitiy/UserCollectAlbum.java
+++ backend/LILAC-Backend/src/main/java/com/lilacmusic/backend/albums/model/entitiy/UserCollectAlbum.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
 import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
@@ -19,16 +20,20 @@ public class UserCollectAlbum {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_collect_album_id", unique = true, nullable = false)
+    @Comment("소장앨범 관계 PK")
     private Long userCollectAlbumId;
 
     @Column(name = "user_id", nullable = false)
+    @Comment("소장한 유저 PK")
     private Long userId;
 
     @Column(name = "album_id", nullable = false)
+    @Comment("소장된 앨범 PK")
     private Long albumId;
 
     @CreatedDate
     @Column(name = "created_time", nullable = false)
+    @Comment("소장 시각")
     private LocalDateTime createdTime;
 
 }
