23e8ff6472e5fd7ffa50d867e6c024123887468e
김명준 <audwnswmf@naver.com>
2023-04-25 11:28:07 +0900

    ✨ Feat: 앨범 상세 API 추가

--- backend/LILAC-Backend/src/main/java/com/lilacmusic/backend/albums/controller/AlbumController.java
+++ backend/LILAC-Backend/src/main/java/com/lilacmusic/backend/albums/controller/AlbumController.java
@@ -1,7 +1,9 @@
 package com.lilacmusic.backend.albums.controller;
 
+import com.lilacmusic.backend.albums.dto.response.AlbumDetailResponse;
 import com.lilacmusic.backend.albums.dto.response.CollectedAlbumListResponse;
 import com.lilacmusic.backend.albums.dto.response.ReleasedAlbumListResponse;
+import com.lilacmusic.backend.albums.exceptions.NoAlbumFoundException;
 import com.lilacmusic.backend.albums.service.AlbumService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,12 +29,23 @@ public class AlbumController {
 
     @GetMapping("/collected/{pageNumber}")
     public ResponseEntity<CollectedAlbumListResponse> getCollectedAlbums(@PathVariable("pageNumber") Integer pageNumber,
-                                                                        @RequestHeader HttpHeaders headers){
+                                                                        @RequestHeader HttpHeaders headers) {
 //        Long userId = getUserIdByAccessToken(headers.get("Authorization"));
         Long userId = 1L;
 
         CollectedAlbumListResponse response = albumService.getCollectedAlbums(pageNumber, userId);
         return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/{albumCode}")
+    public ResponseEntity<AlbumDetailResponse> getAlbumDetail(@PathVariable("albumCode") String albumCode,
+                                                              @RequestHeader HttpHeaders headers) throws NoAlbumFoundException {
+//        Long userId = getUserIdByAccessToken(headers.get("Authorization"));
+        Long userId = 1L;
+
+        AlbumDetailResponse response = albumService.getAlbumDetail(albumCode, userId);
+        return ResponseEntity.ok().body(response);
+
     }
 
 }
--- backend/LILAC-Backend/src/main/java/com/lilacmusic/backend/musics/model/repository/MusicRepository.java
+++ backend/LILAC-Backend/src/main/java/com/lilacmusic/backend/musics/model/repository/MusicRepository.java
@@ -3,5 +3,9 @@ package com.lilacmusic.backend.musics.model.repository;
 import com.lilacmusic.backend.musics.model.entity.Music;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MusicRepository extends JpaRepository<Music, Long> {
+
+    List<Music> getAllByAlbumIdOrderByMusicIndex(Long albumId);
 }
