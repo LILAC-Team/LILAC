7dfae0696e7e210e29c1280480ba4ae86e3637b4
young <moxnox63@naver.com>
2023-04-25 14:30:39 +0900

    ✨ Feat: ProfileImg Component 추가
    - [x] ProfileImg Component 생성
    - [x] Props 변경사항 적용: Notion 확인

--- frontend/components/common/ProfileImg/index.tsx
+++ frontend/components/common/ProfileImg/index.tsx
@@ -5,18 +5,20 @@ import { MdModeEdit } from "@react-icons/all-files/md/MdModeEdit";
 interface ProfileImgProps {
   src?: string;
   size: number;
+  onClickEvent: () => void;
   isEditable?: boolean;
 }
 
 const ProfileImg = ({
   src = "/defaultProfile.svg",
   size,
+  onClickEvent,
   isEditable,
 }: ProfileImgProps) => {
   return (
     <Fragment>
       {isEditable ? (
-        <S.ProfileImage src={src} size={size}>
+        <S.ProfileImage src={src} size={size} onClick={onClickEvent}>
           <S.EditIconWrapper>
             <label htmlFor="featured-image">
               <MdModeEdit className="edit-icon" size={20} />
@@ -30,7 +32,7 @@ const ProfileImg = ({
           </S.EditIconWrapper>
         </S.ProfileImage>
       ) : (
-        <S.ProfileImage src={src} size={size} />
+        <S.ProfileImage src={src} size={size} onClick={onClickEvent} />
       )}
     </Fragment>
   );
