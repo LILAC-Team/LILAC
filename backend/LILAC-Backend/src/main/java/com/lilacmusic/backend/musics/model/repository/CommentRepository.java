db9996c4ac71009b1dc432d32950c3b0be3f77b6
김명준 <audwnswmf@naver.com>
2023-04-25 15:24:26 +0900

    ✨ Feat: 전체 댓글 API 추가

--- backend/LILAC-Backend/src/main/java/com/lilacmusic/backend/musics/model/repository/CommentRepository.java
+++ backend/LILAC-Backend/src/main/java/com/lilacmusic/backend/musics/model/repository/CommentRepository.java
@@ -1,7 +1,16 @@
 package com.lilacmusic.backend.musics.model.repository;
 
 import com.lilacmusic.backend.musics.model.entity.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query("SELECT c.code, c.content, c.presentTime, c.createdTime, u.nickname, u.profileImage FROM Comment c INNER JOIN User u ON c.userId = u.userId WHERE c.musicId = ?1")
+    Page<Object[]> findAllByMusicId(Long musicId, Pageable pageable);
+
 }
