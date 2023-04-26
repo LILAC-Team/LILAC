1b7d84d3b0520827115095f9a34eeeac3c76200d
young <moxnox63@naver.com>
2023-04-26 15:09:07 +0900

    🚧 Progress: Header component 디자인 작업중
    - [x] Header component 로고 크기 수정

--- frontend/components/common/Header/index.tsx
+++ frontend/components/common/Header/index.tsx
@@ -3,15 +3,13 @@ import BasicText from "../BasicText";
 import ProfileImg from "../ProfileImg";
 import * as S from "./style";
 
-const Header = () => {
-  const [isUser, setIsUser] = useState<boolean>(true);
+interface HeaderProps {
+  isShown?: boolean;
+}
 
+const Header = ({ isShown = true }: HeaderProps) => {
   const handleProfileClick = () => {
-    if (isUser) {
-      console.log("Navigate to User Profile Page");
-    } else {
-      console.log("Navigate to Landing Page");
-    }
+    console.log("Navigate to User Profile Page");
   };
   const handleLogoClick = () => {
     console.log("Navigate to Main Page");
@@ -22,13 +20,15 @@ const Header = () => {
       <S.LogoWrapper onClick={handleLogoClick}>
         <BasicText
           text="LILAC"
-          size="5rem"
           font=""
-          color="linear-gradient(0deg, rgba(216,194,254,1) 0%, rgba(204,164,252,1) 35%, rgba(61,58,75,1) 100%)"
+          background="linear-gradient(0deg, rgba(61,58,75,1) 0%, rgba(204,164,252,1) 55%, rgba(216,194,254,1) 100%)"
+          color="transparent"
+          clip={true}
         />
       </S.LogoWrapper>
-      <ProfileImg size="10%" onClickEvent={handleProfileClick} />
-      {/* <S.ProfileWrapper></S.ProfileWrapper> */}
+      <S.ProfileWrapper>
+        {isShown && <ProfileImg onClickEvent={handleProfileClick} />}
+      </S.ProfileWrapper>
     </S.HeaderWrapper>
   );
 };
--- frontend/components/common/Header/style.tsx
+++ frontend/components/common/Header/style.tsx
@@ -1,17 +1,20 @@
 import styled from "styled-components";
 
 export const HeaderWrapper = styled.div`
-  border: 2px solid red;
+  /* border: 2px solid red; */
 
   display: flex;
   width: 100%;
   height: 10%;
+  padding: 5% 2.5% 0 2.5%;
   align-items: center;
   justify-content: space-between;
 `;
 
 export const LogoWrapper = styled.div`
-  margin-left: 2%;
+  width: 4rem;
 `;
 
-export const ProfileWrapper = styled.div``;
+export const ProfileWrapper = styled.div`
+  width: 3rem;
+`;
