d6ed506ba9bdc4ef85b372171dd0535ce8e63970
김명준 <audwnswmf@naver.com>
2023-04-26 16:07:17 +0900

    ♻️ Refactor: 음원 아이디 찾는 서비스 분리 및 음원 상세에 앨범커버 추가

--- backend/LILAC-Backend/src/main/java/com/lilacmusic/backend/musics/service/MusicServiceImpl.java
+++ backend/LILAC-Backend/src/main/java/com/lilacmusic/backend/musics/service/MusicServiceImpl.java
@@ -25,13 +25,13 @@ public class MusicServiceImpl implements MusicService {
 
     @Override
     public MusicDetailResponse getMusicDetail(String musicCode, Long userId) throws NoMusicFoundException {
-        Optional<Music> optionalMusic = musicRepository.findByCode(musicCode);
+        Optional<Object[]> optionalMusic = musicRepository.findByCodeWithAlbumImage(musicCode);
         if (optionalMusic.isEmpty()) {
             throw new NoMusicFoundException();
         }
-        Music music = optionalMusic.get();
+
         // 유저의 음원 소유 확인 여부 로직 추가할건지???????????
-        List<Object[]> recentComments = recentCommentRepository.findAllByMusicIdOrderByPresentTimeAsc(music.getMusicId());
+        List<Object[]> recentComments = recentCommentRepository.findAllByMusicIdOrderByPresentTimeAsc((Long) optionalMusic.get()[0]);
         List<RecentCommentResponse> recentCommentResponseList = recentComments.stream().map(c ->
                 RecentCommentResponse.builder()
                         .content((String) c[0])
@@ -41,13 +41,23 @@ public class MusicServiceImpl implements MusicService {
         ).collect(Collectors.toList());
         MusicDetailResponse response = MusicDetailResponse.builder()
                 .recentCommentList(recentCommentResponseList)
-                .code(music.getCode())
-                .name(music.getName())
-                .artistName(music.getArtistName())
-                .playtime(music.getPlaytime())
-                .storagePath(music.getStoragePath())
+                .code((String) optionalMusic.get()[1])
+                .name((String) optionalMusic.get()[2])
+                .artistName((String) optionalMusic.get()[3])
+                .playtime((Integer) optionalMusic.get()[4])
+                .storagePath((String) optionalMusic.get()[5])
+                .albumImage((String) optionalMusic.get()[6])
                 .build();
 
         return response;
+    }
+
+    @Override
+    public Long getMusicIdByCode(String code) throws NoMusicFoundException {
+        Optional<Long> musicId = musicRepository.findMusicIdByCode(code);
+        if (musicId.isEmpty()) {
+            throw new NoMusicFoundException();
+        }
+        return musicId.get();
     }
 }
