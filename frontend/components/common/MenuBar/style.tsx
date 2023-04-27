cdbada2a468b8e81c75db20d58abd7000aa8e2fa
young <moxnox63@naver.com>
2023-04-27 15:37:51 +0900

    ✨ Feat: MenuBar component 생성
    - [x] MenuBar component 생성
    - [x] handleClickEvent 상세 내용 추후 작업
    - [x] Test 파일 삭제

---
+++ frontend/components/common/MenuBar/style.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const MenuWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  background-color: #676767;
+
+  position: absolute;
+  left: 50%;
+  bottom: 0;
+  transform: translateX(-50%);
+
+  width: 100%;
+  max-width: 900px;
+  height: 3.5rem;
+  align-items: center;
+  justify-items: center;
+  border-top-left-radius: 1rem;
+  border-top-right-radius: 1rem;
+`;
