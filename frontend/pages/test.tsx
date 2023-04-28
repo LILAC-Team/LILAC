0e9b9456054d38d96bdb5104a70d93019ad4a66a
yunji9151 <yunji9151@naver.com>
2023-04-28 17:10:18 +0900

    💡 Comment: 주석 삭제
    1. Test 파일 불필요한 주석 삭제

--- frontend/pages/test.tsx
+++ frontend/pages/test.tsx
@@ -1,12 +1,10 @@
 import BasicText from "@/components/common/BasicText";
-import React, { useState } from "react";
+import React from "react";
 import { useRouter } from "next/router";
 import test from "./test.json";
 import CustomTextButton from "@/components/common/CustomTextButton";
 import CustomIconButton from "@/components/common/CustomIconButton";
 import { AiOutlinePlus } from "react-icons/ai";
-import { render } from "react-dom";
-// import Drawer from "react-drag-drawer";
 import AlbumCard from "@/components/common/AlbumCard";
 import BasicImage from "@/components/common/BasicImage";
 import styled, { css } from "styled-components";
@@ -44,55 +42,8 @@ const Test = () => {
   const iOS =
     typeof navigator !== "undefined" &&
     /iPad|iPhone|iPod/.test(navigator.userAgent);
-  // const [state, setState] = React.useState({
-  //   regular: false,
-  //   sidebarLeft: false,
-  //   sidebarRight: false,
-  //   asyncHeight: false,
-  //   crazyStyle: false,
-  // });
-
-  // const toggle =
-  //   (type: string, value: boolean) =>
-  //   (event: React.KeyboardEvent | React.MouseEvent) => {
-  //     setState({ ...state, [type]: value });
-  //   };
-
-  // const { regular, sidebarLeft, sidebarRight, asyncHeight, crazyStyle } = state;
-
-  // const ModalElement = css`
-  //   position: absolute;
-  //   top: 30px;
-  // `;
   return (
     <>
-      {/* <BasicText
-        text="테스트"
-        // color="transparent"
-        // background="linear-gradient(0deg, rgba(216,194,254,1) 0%, rgba(204,164,252,1) 35%, rgba(61,58,75,1) 100%)"
-        font=""
-        // clip={true}
-        size="2rem"
-      /> */}
-      <BasicImage
-        src="https://i.namu.wiki/i/m60BZ35BZRrbiqpurjIPB7GAs74I2LPNXe0MuHeEemha3ksZzGJo21PfgIdXn6JXZV0Wnps6xiAMPCVb_BYIwMeDwGEtL1R9Sxe5lmGUb4ZlPMyUO-vxTNG-6RMTR23h-myh5DqQk0h38DUi-wxiUA.jpg"
-        size="15rem"
-        radius={10}
-        isRotate={true}
-      />
-
-      {/* <button onClick={toggle("regular", true)}>Open example</button>
-
-      <Drawer
-        open={regular}
-        onRequestClose={toggle("regular", false)}
-        // modalElementclass={ModalElement}
-      >
-        <Modaldiv>
-          hey
-          <button onClick={toggle("regular", false)}>Close drawer</button>
-        </Modaldiv>
-      </Drawer> */}
       {(["bottom"] as const).map((anchor) => (
         <React.Fragment key={anchor}>
           <button onClick={toggleDrawer(anchor, true)}>{anchor}</button>
@@ -109,6 +60,14 @@ const Test = () => {
           </SwipeableDrawer>
         </React.Fragment>
       ))}
+      <BasicText text="테스트" size="2rem" />
+      <BasicImage
+        src="https://i.namu.wiki/i/m60BZ35BZRrbiqpurjIPB7GAs74I2LPNXe0MuHeEemha3ksZzGJo21PfgIdXn6JXZV0Wnps6xiAMPCVb_BYIwMeDwGEtL1R9Sxe5lmGUb4ZlPMyUO-vxTNG-6RMTR23h-myh5DqQk0h38DUi-wxiUA.jpg"
+        size="15rem"
+        radius={10}
+        isRotate={true}
+      />
+
       <div style={tmpStyle}>
         {test.releasedAlbumList.map((item) => {
           return (
@@ -127,9 +86,6 @@ const Test = () => {
           size="2rem"
           font=""
           fontColor="#ffffff"
-          // border="5px dashed green"
-          // isBackground={false}
-          // isDisabled={true}
           handleOnClickButton={tmpFunction}
         />
       </div>
@@ -137,8 +93,6 @@ const Test = () => {
         <CustomIconButton
           color="#d47a7a"
           size="5rem"
-          // border="5px dashed black"
-          // isDisabled={true}
           handleOnClickButton={tmpFunction}
         >
           <AiOutlinePlus color="#ffffff" size="5rem" />
@@ -150,27 +104,6 @@ const Test = () => {
 
 export default Test;
 
-const modal = css`
-  position: absolute;
-  top: 30px;
-  background-color: white;
-  width: 100%;
-  max-width: 700px;
-  min-height: 100%;
-  border-top-left-radius: 4px;
-  border-top-right-radius: 4px;
-`;
-
-export const Modaldiv = styled.div`
-  /* position: absolute; */
-  width: calc(var(--vw, 1vw) * 100);
-  height: calc(var(--vh, 1vh) * 95);
-  background-color: black;
-  /* top: 30px; */
-  /* top: -calc(var(--vh, 1vh) * 50); */
-  /* bottom: 0px; */
-`;
-
 export const MyDiv = styled.div`
   width: calc(var(--vw, 1vw) * 100);
   height: calc(var(--vh, 1vh) * 95);
@@ -181,16 +114,3 @@ export const MyDiv = styled.div`
   align-items: center;
   text-align: center;
 `;
-
-export const modalItem = styled.div`
-  position: absolute;
-  top: 30px;
-`;
-
-// export const Container = styled.div`
-//   .ModalElement {
-//     position: absolute;
-//     top: 30px;
-//     text-align: center;
-//   }
-// `;
