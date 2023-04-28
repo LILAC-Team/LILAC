f1d944b154a558a76f09137298d9f4bc6cdae5e5
young <moxnox63@naver.com>
2023-04-28 17:01:04 +0900

    ✨ Feat: MusicPlayerBar component 생성
    - [x] MusicPlayerBar component CSS 완료
    - [x] API 연결 및 로직 작성 전

--- frontend/components/common/Layout/index.tsx
+++ frontend/components/common/Layout/index.tsx
@@ -2,16 +2,29 @@ import React from "react";
 import Header from "../Header";
 import styled from "styled-components";
 import NavigationBar from "../NavigationBar";
+import MusicPlayerBar from "@/components/Player/MusicPlayerBar";
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const data = {
+    name: "Hype Boy",
+    albumImage:
+      "https://i.namu.wiki/i/m60BZ35BZRrbiqpurjIPB7GAs74I2LPNXe0MuHeEemha3ksZzGJo21PfgIdXn6JXZV0Wnps6xiAMPCVb_BYIwMeDwGEtL1R9Sxe5lmGUb4ZlPMyUO-vxTNG-6RMTR23h-myh5DqQk0h38DUi-wxiUA.jpg",
+    code: "bbbb",
+    releasedDate: "2023-04-24T01:00:00",
+    nickname: "봄윤식스",
+  };
+
   return (
     <>
       <Header />
       <ChildrenWrap>{children}</ChildrenWrap>
-      <TempWrap>절 지워주세요.</TempWrap>
+      {/* <TempWrap>절 지워주세요.</TempWrap> */}
+      <TempWrap>
+        <MusicPlayerBar data={data} />
+      </TempWrap>
       <NavigationBar />
     </>
   );
