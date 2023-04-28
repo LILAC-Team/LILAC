06a2e3ac5c605a3f98fa5da68e0d4e125e4c90a6
young <moxnox63@naver.com>
2023-04-28 14:27:10 +0900

    🚧 Progress: MusicController component CSS 완료
    - [x] MusicList Redux 호출 필요
    - [x] ClickEvent 로직 추가 필요

--- frontend/components/Player/MusicController/style.tsx
+++ frontend/components/Player/MusicController/style.tsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const ControllerWrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-
   width: 100%;
   max-width: 900px;
 `;
