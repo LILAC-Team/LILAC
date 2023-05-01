31922011732fd7108e801dce8e83487552a920a7
yunji9151 <yunji9151@naver.com>
2023-05-01 17:41:24 +0900

    🚧 Progress: MusicPlayerDrawer 코드 작성중
    1. MusicPlayerDrawer 코드 작성중

--- frontend/components/Player/MusicPlayerDrawer/style.tsx
+++ frontend/components/Player/MusicPlayerDrawer/style.tsx
@@ -10,27 +10,63 @@ export const Player = styled.div`
   text-align: center;
 `;
 
+export const Top = styled.div`
+  padding-top: calc((var(--vh, 1vh) * 2));
+  padding-bottom: calc((var(--vh, 1vh) * 2));
+  display: flex;
+  justify-content: center;
+`;
+
 export const AlbumCover = styled.div`
-  /* padding: 2rem; */
+  padding: calc((var(--vh, 1vh) * 2.5)) calc((var(--vw, 1vw) * 5))
+    calc((var(--vh, 1vh) * 2.5)) calc((var(--vw, 1vw) * 2.5));
 `;
 
 export const Title = styled.div`
   padding: 1rem;
   justify-content: center;
-  align-items: center;
+  display: flex;
 `;
 
 export const Artist = styled.div`
   padding: 0.5rem;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Comment = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+`;
+
+export const CommentImg = styled.div`
+  display: flex;
+  padding: 0.25rem;
+  align-items: center;
+`;
+
+export const CommentDiv = styled.div`
+  display: flex;
+  padding: 0.25rem;
+  align-items: center;
 `;
 
 export const MusicBar = styled.div`
+  display: flex;
   padding: 0.5rem;
+  position: absolute;
+  justify-content: center;
+  width: 100%;
+  bottom: calc((var(--vh, 1vh) * 22.5));
 `;
 
 export const ControllBar = styled.div`
+  display: flex;
   width: 100%;
-  padding: 0.5rem;
+  padding: 1rem;
   position: absolute;
-  bottom: 5rem;
+  bottom: calc((var(--vh, 1vh) * 12.5));
 `;
