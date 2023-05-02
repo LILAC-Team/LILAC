680f6d15685bbbcf28bb90494952117b595e9ffb
yunji9151 <yunji9151@naver.com>
2023-05-02 15:19:42 +0900

    🚧 Progress: MusicPlayerDrawer 로직 수정
    1. Drawer 로직 변경에 따른 코드 추가
    2. CSS 일부 수정

--- frontend/components/Player/MusicPlayerDrawer/index.tsx
+++ frontend/components/Player/MusicPlayerDrawer/index.tsx
@@ -6,6 +6,7 @@ import MenuBar from "@/components/common/MenuBar";
 import BasicText from "@/components/common/BasicText";
 import MusicController from "../MusicController";
 import styled, { css } from "styled-components";
+import Drawer from "@/components/common/Drawer";
 
 const MusicPlayerDrawer = () => {
   const [nowTime, setNowTime] = useState(5);
@@ -15,10 +16,29 @@ const MusicPlayerDrawer = () => {
     setIsRotating(!state);
   };
 
+  const [state, setState] = React.useState({ bottom: false });
+  const toggleDrawer =
+    (anchor: string, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
+  const iOS =
+    typeof navigator !== "undefined" &&
+    /iPad|iPhone|iPod/.test(navigator.userAgent);
+
   return (
     <S.Player>
       <S.Top>
-        <BasicText text="Now Playing" size="125%" />
+        <BasicText text="Now Playing" size="125%" font="NotoSansKR500" />
       </S.Top>
       <S.AlbumCover>
         <BasicImage
@@ -29,28 +49,35 @@ const MusicPlayerDrawer = () => {
         />
       </S.AlbumCover>
       <S.Title>
-        <BasicText text={music.name} size="2rem" />
+        <BasicText text={music.name} size="2rem" font="NotoSansKR700" />
       </S.Title>
       <S.Artist>
-        <BasicText text={music.artistName} size="1.25rem" />
+        <BasicText text={music.artistName} size="1rem" font="NotoSansKR400" />
       </S.Artist>
       <S.Comment>
         {music.recentCommentList.map((item, index) => {
           return (
             <>
               {item.present_time === nowTime && (
-                <S.Comment key={index}>
+                <S.CommentWrap
+                  key={index}
+                  onClick={toggleDrawer("bottom", true)}
+                >
                   <S.CommentImg>
                     <BasicImage
                       src={item.userInfo.profileImage}
-                      size="2rem"
+                      size="1.5rem"
                       radius={100}
                     />
                   </S.CommentImg>
                   <S.CommentDiv>
-                    <BasicText text={item.content} size="1rem" />
+                    <BasicText
+                      text={item.content}
+                      size="0.75rem"
+                      font="NotoSansKR400"
+                    />
                   </S.CommentDiv>
-                </S.Comment>
+                </S.CommentWrap>
               )}
             </>
           );
@@ -63,6 +90,12 @@ const MusicPlayerDrawer = () => {
         <MusicController handleRotateClick={handleRotateClick} />
       </S.ControllBar>
       <MenuBar />
+      <Drawer
+        inner="comment"
+        toggleDrawer={toggleDrawer}
+        state={{ ...state }}
+        anchor={"bottom"}
+      />
     </S.Player>
   );
 };
