5c77984c094a587efa46daf7e5e65b36e9e8dcbb
yunji9151 <yunji9151@naver.com>
2023-05-02 15:43:48 +0900

    ✨ Feat: CommentDrawer 추가
    1. CommentDrawer 추가

---
+++ frontend/components/Player/CommentDrawer/index.tsx
@@ -0,0 +1,12 @@
+import BasicText from "@/components/common/BasicText";
+import * as S from "./style";
+
+const CommentDrawer = () => {
+  return (
+    <S.Comment>
+      <BasicText text="Comment" size="125%" font="NotoSansKR500" />
+    </S.Comment>
+  );
+};
+
+export default CommentDrawer;
