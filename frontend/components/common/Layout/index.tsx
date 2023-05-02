88bc1808e4de336adaf260cb635aed247dc35fef
yunji9151 <yunji9151@naver.com>
2023-05-02 15:33:06 +0900

    🚧 Progress: Layout의 MusicPlayerBar onClick 이벤트 추가
    1. MusicPlayerBar Toggle 이벤트 추가

--- frontend/components/common/Layout/index.tsx
+++ frontend/components/common/Layout/index.tsx
@@ -3,6 +3,8 @@ import Header from "../Header";
 import styled from "styled-components";
 import NavigationBar from "../NavigationBar";
 import MusicPlayerBar from "@/components/Player/MusicPlayerBar";
+import Drawer from "@/components/common/Drawer";
+
 interface LayoutProps {
   children: React.ReactNode;
 }
@@ -17,11 +19,35 @@ const Layout = ({ children }: LayoutProps) => {
     nickname: "봄윤식스",
   };
 
+  const [state, setState] = React.useState({ bottom: false });
+  const toggleDrawer =
+    (anchor: string, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
+  const iOS =
+    typeof navigator !== "undefined" &&
+    /iPad|iPhone|iPod/.test(navigator.userAgent);
   return (
     <ContainerWrap>
       <Header />
       <ChildrenWrap>{children}</ChildrenWrap>
-      <MusicPlayerBar data={data} />
+      <MusicPlayerBar data={data} onClickEvent={toggleDrawer("bottom", true)} />
+      <Drawer
+        inner="player"
+        toggleDrawer={toggleDrawer}
+        state={{ ...state }}
+        anchor={"bottom"}
+      />
       <NavigationBar />
     </ContainerWrap>
   );
