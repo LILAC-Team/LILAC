b4c216a170a917da3681f6ec4f94764d7392a690
yunji9151 <yunji9151@naver.com>
2023-05-03 17:32:46 +0900

    ✨ Feat: CommentInput Components 추가
    1. CommentInput Components 추가

---
+++ frontend/components/Player/CommentInput/index.tsx
@@ -0,0 +1,65 @@
+import * as S from "./style";
+import { useEffect, useState } from "react";
+import BasicText from "@/components/common/BasicText";
+import BasicImage from "@/components/common/BasicImage";
+import CustomIconButton from "@/components/common/CustomIconButton";
+import { BsFillSendFill } from "react-icons/bs";
+import ProfileImg from "@/components/common/ProfileImg";
+
+interface CommentInputProps {
+  src: string;
+  value: string;
+  handleOnChangeValue: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleOnKeyDownValue: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  handleOnClickValue: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+const CommentInput = ({
+  src,
+  value,
+  handleOnChangeValue,
+  handleOnKeyDownValue,
+  handleOnClickValue,
+}: CommentInputProps) => {
+  const [nowTime, setNowTime] = useState(178);
+  const [convertTime, setConvertTime] = useState("");
+
+  useEffect(() => {
+    setConvertTime(
+      (nowTime - (nowTime % 60)) / 60 +
+        " : " +
+        (nowTime % 60 >= 10 ? nowTime % 60 : "0" + (nowTime % 60))
+    );
+  }, [nowTime]);
+  return (
+    <S.CommentInput>
+      <S.InputImg>
+        <ProfileImg src={src} />
+      </S.InputImg>
+      <S.InputWrap>
+        <S.Input
+          type="text"
+          value={value}
+          onChange={handleOnChangeValue}
+          onKeyDown={handleOnKeyDownValue}
+          placeholder="댓글을 입력하세요"
+        />
+        <S.InputTime>
+          <BasicText
+            text={convertTime}
+            color="#929292"
+            size="0.75rem"
+            font="NotoSansKR400"
+          />
+        </S.InputTime>
+        <S.CommentBtn>
+          <CustomIconButton handleOnClickButton={handleOnClickValue}>
+            <BsFillSendFill color="black" />
+          </CustomIconButton>
+        </S.CommentBtn>
+      </S.InputWrap>
+    </S.CommentInput>
+  );
+};
+
+export default CommentInput;
---
+++ frontend/components/Player/CommentInput/style.tsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const CommentInput = styled.div`
+  width: calc((var(--vw, 1vw) * 100));
+  height: calc((var(--vh, 1vh) * 10));
+  padding: calc((var(--vh, 1vh) * 2)) calc((var(--vw, 1vw) * 7.5))
+    calc((var(--vh, 1vh) * 2)) calc((var(--vw, 1vw) * 7.5));
+  display: flex;
+  align-items: center;
+`;
+
+export const InputImg = styled.div`
+  width: calc((var(--vh, 1vh) * 7.5));
+  margin-left: calc((var(--vw, 1vw) * 0.5));
+  margin-right: calc((var(--vw, 1vw) * 2.5));
+`;
+
+export const InputWrap = styled.div`
+  width: 100%;
+  height: 80%;
+  display: flex;
+  background-color: white;
+  border-radius: 1rem;
+  padding: 0 calc((var(--vw, 1vw) * 2.5)) 0 calc((var(--vw, 1vw) * 2.5));
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  background-color: transparent;
+  border: none;
+  font-family: "RidiBatang";
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const InputTime = styled.div`
+  width: 5rem;
+  padding-bottom: 0.125rem;
+  display: flex;
+  justify-content: center;
+`;
+
+export const CommentBtn = styled.div`
+  width: 3rem;
+  /* margin-right: 1rem; */
+`;
