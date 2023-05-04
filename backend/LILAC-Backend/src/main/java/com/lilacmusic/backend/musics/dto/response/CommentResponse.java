4b649ae95cdcd4553de3f07f4502b63c6a07fdee
김명준 <audwnswmf@naver.com>
2023-05-04 16:28:16 +0900

    ♻️ Refactor: 요청용 dto에 validation 추가

--- backend/LILAC-Backend/src/main/java/com/lilacmusic/backend/musics/dto/response/CommentResponse.java
+++ backend/LILAC-Backend/src/main/java/com/lilacmusic/backend/musics/dto/response/CommentResponse.java
@@ -1,5 +1,6 @@
 package com.lilacmusic.backend.musics.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.lilacmusic.backend.albums.dto.response.MemberInfoResponse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,6 +21,7 @@ public class CommentResponse {
 
     private Integer presentTime;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime createdTime;
 
     private MemberInfoResponse memberInfo;
--- backend/LILAC-Backend/src/main/java/com/lilacmusic/backend/playlists/dto/request/PlayListAddRequest.java
+++ backend/LILAC-Backend/src/main/java/com/lilacmusic/backend/playlists/dto/request/PlayListAddRequest.java
@@ -5,10 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class PlayListAddRequest {
+    @Size(max = 100)
     private String code;
 }
