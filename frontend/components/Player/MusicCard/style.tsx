7691e0ce4919c5ea58f1865bac97ca6067da0b7a
yunji9151 <yunji9151@naver.com>
2023-05-04 17:07:33 +0900

    🎨 Design: 요소 추가에 따른 CSS 수정
    1. CSS 추가 & 수정

--- frontend/components/Player/MusicCard/style.tsx
+++ frontend/components/Player/MusicCard/style.tsx
@@ -6,8 +6,8 @@ export const MusicCard = styled.div`
   max-height: 5rem;
   justify-content: center;
   justify-content: space-between;
-  height: calc((var(--vw, 1vw) * 18));
-  padding: calc((var(--vw, 1vw) * 2));
+  height: calc((var(--vw, 1vw) * 20.5));
+  padding: calc((var(--vh, 1vh) * 2)) 0 calc((var(--vh, 1vh) * 2)) 0;
 `;
 
 export const LeftWrapper = styled.div`
@@ -29,18 +29,50 @@ export const TextWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   margin-left: calc((var(--vw, 1vw) * 5));
+  width: 80%;
+`;
+
+export const TopWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
 `;
 
 export const Title = styled.div`
+  /* width: 100%; */
   height: 100%;
   justify-content: center;
   padding-bottom: calc((var(--vw, 1vw) * 0.5));
+`;
+
+export const isTitle = styled.div`
+  background-color: #e3dfff;
+  box-shadow: 3px 3px 3px black;
+  height: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 0.75rem;
+  width: 2.5rem;
+  border-radius: 1rem;
+`;
+
+export const BottomWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
 `;
 
 export const Singer = styled.div`
   height: 100%;
   justify-content: center;
   padding-top: calc((var(--vw, 1vw) * 0.5));
+`;
+
+export const Time = styled.div`
+  /* width: 100%; */
+  margin-left: 0.75rem;
+  padding-top: 0.35rem;
 `;
 
 export const Hamberger = styled.div`
