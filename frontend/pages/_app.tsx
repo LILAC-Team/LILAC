e7307d1d89dee881e72578aacc35328d74443c65
young <moxnox63@naver.com>
2023-05-04 14:38:57 +0900

    ✨ Feat: Redux 및 Persist 환경 설정
    - [x] RTK, redux-persist, next-redux-wrapper 설치
    - [x] store 폴더 하위에 slices 저장을 위한 modules 폴더 생성
    - [x] configStore 파일에 store 및 reducers 설정
    - [x] -app.tsx에 redux 데이터 Provider로 제공

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,12 @@
 import type { AppProps } from "next/app";
 import { GlobalStyle } from "@/styles/globalStyle";
 import { useEffect } from "react";
+import { Provider } from "react-redux";
+import wrapper from "@/store/configStore";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const { store, props } = wrapper.useWrappedStore(pageProps);
+
   useEffect(() => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
@@ -12,11 +16,12 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       document.documentElement.style.setProperty("--vh", `${vh}px`);
     });
   }, []);
+
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
-      <Component {...pageProps} />
-    </>
+      <Component {...props} />
+    </Provider>
   );
 };
 
---
+++ frontend/store/configStore.ts
@@ -0,0 +1,38 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import { createWrapper, HYDRATE } from "next-redux-wrapper";
+import storage from "redux-persist/lib/storage";
+
+import user from "./modules/user/index";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["user"],
+};
+
+const rootReducer = combineReducers({
+  user,
+});
+
+const reducers = persistReducer(persistConfig, rootReducer);
+
+export const makeStore = () => {
+  const store = configureStore({
+    reducer: (state, action) => {
+      switch (action.type) {
+        case HYDRATE:
+          return action.payload;
+        default:
+          return reducers(state, action);
+      }
+    },
+  });
+  return store;
+};
+
+const wrapper = createWrapper(makeStore, {
+  debug: process.env.NODE_ENV === "development",
+});
+
+export default wrapper;
