5d323abc39966def04c146a54182a8110e18c611
hyunsu <gustn913@naver.com>
2023-05-07 22:59:13 +0900

    :bug: fix: axios interceptor 쿠키 관련 에러 수정.
    쿠키 정보를 못읽어오는 문제 수정.

--- frontend/api/index.tsx
+++ frontend/api/index.tsx
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { useCookies } from "react-cookie";
+import { Cookies } from "react-cookie";
+
+const cookies = new Cookies();
 
 const BASE_URL = "https://lilac-music.net/api/v1";
 const instance = axios.create({
@@ -9,64 +11,61 @@ const instance = axios.create({
   },
 });
 
-// instance.interceptors.request.use(function (config) {
-//   console.log("interceptor request");
-//   const accessToken = sessionStorage.getItem("accessToken");
+instance.interceptors.request.use(function (config) {
+  const accessToken = cookies.get("accessToken");
+  if (!accessToken) {
+    config.headers.Authorization = null;
+    return config;
+  }
 
-//   if (!accessToken) {
-//     config.headers.Authorization = null;
-//     return config;
-//   }
+  if (config.headers && accessToken) {
+    config.headers.Authorization = `${accessToken}`;
+    return config;
+  }
+});
 
-//   if (config.headers && accessToken) {
-//     config.headers.Authorization = `${accessToken}`;
-//     return config;
-//   }
-// });
+instance.interceptors.response.use(
+  (response) => {
+    console.log("interceptor response 200");
+    return response;
+  },
 
-// instance.interceptors.response.use(
-//   (response) => {
-//     console.log("interceptor response 200");
-//     return response;
-//   },
+  async (error) => {
+    const {
+      config,
+      response: { status },
+    } = error;
 
-//   async (error) => {
-//     const {
-//       config,
-//       response: { status },
-//     } = error;
+    // statusCode 401 : 만료된 토큰
+    if (status === 401) {
+      const accessToken = cookies.get("accessToken");
+      const refreshToken = cookies.get("refreshToken");
+      const originalRequest = config;
 
-//     // statusCode 401 : 만료된 토큰
-//     if (status === 401) {
-//       const [cookies, setCookie] = useCookies(["refreshToken"]);
-//       const originalRequest = config;
-//       const refreshToken = cookies;
+      // 토큰 재발급을 위한 요청
+      if (refreshToken) {
+        const token = sessionStorage.getItem("accessToken");
+        const data = await axios.post(
+          `${BASE_URL}/refresh`,
+          {
+            refreshToken: `${refreshToken}`,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
 
-//       // 토큰 재발급을 위한 요청
-//       if (refreshToken) {
-//         const token = sessionStorage.getItem("accessToken");
-//         const data = await axios.post(
-//           `${BASE_URL}/refresh`,
-//           {
-//             refreshToken: `${refreshToken}`,
-//           },
-//           {
-//             headers: {
-//               "Content-Type": "application/json",
-//             },
-//           }
-//         );
+        const accessToken = data;
+        cookies.set("accessToken", accessToken, { path: "/" });
+        originalRequest.headers.Authorization = `${accessToken}`;
+        return axios(originalRequest);
+      }
+    }
 
-//         const accessToken = data;
-//         // await sessionStorage.setItem("accessToken", accessToken);
-
-//         originalRequest.headers.Authorization = `Bearer ${accessToken}`;
-//         return axios(originalRequest);
-//       }
-//     }
-
-//     return Promise.reject(error);
-//   }
-// );
+    return Promise.reject(error);
+  }
+);
 
 export const api = instance;
