7530fd950e3814177899a902ce796255ce7a23f5
young <moxnox63@naver.com>
2023-05-10 15:17:30 +0900

    🎯 Fix: Form 페이지 getServerSideProps 추가
    - [x] Form 페이지 getServerSideProps 추가

--- frontend/pages/album/[albumId]/index.tsx
+++ frontend/pages/album/[albumId]/index.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import React, { useState, useEffect, useCallback } from "react";
+import CircularJSON from "circular-json";
 import * as S from "./style";
 import Layout from "@/components/common/Layout";
 import BasicImage from "@/components/common/BasicImage";
@@ -322,3 +323,12 @@ const AlbumDetail = () => {
 };
 
 export default AlbumDetail;
+
+export async function getServerSideProps({ req }) {
+  const serializedReq = CircularJSON.stringify(req);
+  return {
+    props: {
+      req: serializedReq,
+    },
+  };
+}
