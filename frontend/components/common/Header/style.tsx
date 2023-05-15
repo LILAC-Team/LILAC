d798cb0ca95d4bba6da9857758dfd9cf9c00ae7d
hyunsu <gustn913@naver.com>
2023-05-16 03:15:25 +0900

    :art: Design : padding 관련 CSS 코드 수정.
    .

--- frontend/components/common/Header/style.tsx
+++ frontend/components/common/Header/style.tsx
@@ -4,7 +4,7 @@ export const HeaderWrapper = styled.div`
   display: flex;
   width: 100%;
   height: 4rem;
-  padding: 1rem 1rem;
+  padding: 1rem;
   align-items: center;
   justify-content: space-between;
 `;
@@ -15,5 +15,5 @@ export const LogoWrapper = styled.div`
 
 export const ProfileWrapper = styled.div`
   width: 2.6rem;
-  aspect-ratio: 1 /1;
+  aspect-ratio: 1 / 1;
 `;
