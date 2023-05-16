a36d0cfb3feb8602de3cc457d6d04cfe5a97f71c
김명준 <audwnswmf@naver.com>
2023-05-16 10:05:15 +0900

    ✨ Feat: 캐시 매니저 및 스프링 액추에이터 설정

--- backend/LILAC-Backend/src/main/java/com/lilacmusic/backend/global/config/RedisConfig.java
+++ backend/LILAC-Backend/src/main/java/com/lilacmusic/backend/global/config/RedisConfig.java
@@ -1,15 +1,23 @@
 package com.lilacmusic.backend.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
@@ -41,5 +49,15 @@ public class RedisConfig {
         StringRedisTemplate redisTemplate = new StringRedisTemplate();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
+    }
+
+    @Bean
+    public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(Duration.ofMinutes(60L));
+        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration).build();
     }
 }
--- backend/LILAC-Backend/src/main/java/com/lilacmusic/backend/member/service/MemberServiceImpl.java
+++ backend/LILAC-Backend/src/main/java/com/lilacmusic/backend/member/service/MemberServiceImpl.java
@@ -129,7 +129,7 @@ public class MemberServiceImpl implements MemberService {
      * @return 이메일의 멤버 아이디, 없으면 -1 리턴
      */
     @Override
-    @Cacheable(value = "memberIdCache")
+    @Cacheable(value = "memberIdCache", key = "#email", cacheManager = "redisCacheManager")
     public Long getMemberIdByEmail(String email) {
         if (email == null) {
             return -1L;
