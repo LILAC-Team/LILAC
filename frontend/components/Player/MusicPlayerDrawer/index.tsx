acc620eeb0d66eb8a95e29a19bad0ba8e61280ee
yunji9151 <yunji9151@naver.com>
2023-05-17 19:59:41 +0900

    🎯 Fix: Player Comment 유지 시간 변경
    1. 댓글 작성 시점 ~ +2초까지 보여지게 변경

--- frontend/components/Player/MusicPlayerDrawer/index.tsx
+++ frontend/components/Player/MusicPlayerDrawer/index.tsx
@@ -49,12 +49,12 @@ const MusicPlayerDrawer = () => {
     <S.Player>
       <S.Top>
         <S.Bar />
-        <BasicText text='Now Playing' size='125%' font='NotoSansKR500' />
+        <BasicText text="Now Playing" size="125%" font="NotoSansKR500" />
       </S.Top>
       <S.AlbumCover>
         <BasicImage
           src={currPlayingMusicInfo.albumImage}
-          size='calc((var(--vh, 1vh) * 30))'
+          size="calc((var(--vh, 1vh) * 30))"
           radius={10}
           isRotate={playing}
         />
@@ -62,19 +62,21 @@ const MusicPlayerDrawer = () => {
       <S.Title>
         <BasicText
           text={currPlayingMusicInfo.name}
-          size='2rem'
-          font='NotoSansKR700'
+          size="2rem"
+          font="NotoSansKR700"
         />
       </S.Title>
       <S.Artist>
-        <BasicText text={currPlayingMusicInfo.artistName} size='1rem' />
+        <BasicText text={currPlayingMusicInfo.artistName} size="1rem" />
       </S.Artist>
       <S.Comment>
         {currPlayingMusicInfo &&
           recentCommentList.map((item, index) => {
             return (
               <React.Fragment key={index}>
-                {item.presentTime === time && (
+                {(item.presentTime === time ||
+                  item.presentTime === time + 1 ||
+                  item.presentTime === time + 2) && (
                   <S.CommentWrap
                     onClick={(e) => {
                       toggleDrawer("bottom", true)(e);
@@ -84,12 +86,12 @@ const MusicPlayerDrawer = () => {
                     <S.CommentImg>
                       <BasicImage
                         src={item.memberInfo.profileImage}
-                        size='1.5rem'
+                        size="1.5rem"
                         radius={100}
                       />
                     </S.CommentImg>
                     <S.CommentDiv>
-                      <BasicText text={item.content} size='0.75rem' />
+                      <BasicText text={item.content} size="0.75rem" />
                     </S.CommentDiv>
                   </S.CommentWrap>
                 )}
