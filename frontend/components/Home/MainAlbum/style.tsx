870e1103d0145d230f5d13663c67aabf5210ece8
moxnox63 <moxnox63@gmail.com>
2023-05-17 20:52:39 +0900

    🚧 Progress: 플레이리스트 편집 작업

--- frontend/components/Home/MainAlbum/style.tsx
+++ frontend/components/Home/MainAlbum/style.tsx
@@ -90,6 +90,9 @@ export const CdBox = styled.div`
 export const CdWrapper = styled.div`
   height: 4rem;
   aspect-ratio: 1 / 1;
+  border-radius: 50%;
+  box-shadow: 0 0.875rem 1.75rem rgba(0, 0, 0, 0.25),
+    0 0.625rem 0.625rem rgba(0, 0, 0, 0.22);
 
   flex-direction: column;
   align-items: center;
