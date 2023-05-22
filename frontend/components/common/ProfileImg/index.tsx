7556fc671819cb33c76e3fc4f72401af3ce0b9cb
yunji9151 <yunji9151@naver.com>
2023-05-22 16:10:04 +0900

    🎯 Fix: 기본 이미지 수정
    1. 기본 이미지 수정

--- frontend/components/common/ProfileImg/index.tsx
+++ frontend/components/common/ProfileImg/index.tsx
@@ -10,7 +10,7 @@ interface ProfileImgProps {
 }
 
 const ProfileImg = ({
-  src = "/icons/favicon-96x96.png",
+  src = "/icons/favicon-512x512.png",
   size = "100%",
   onClickEvent,
   isEditable = false,
