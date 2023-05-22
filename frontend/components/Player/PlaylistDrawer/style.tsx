ba452dbfe8f80ede2343ca556d53022e0e48a46f
yunjichoi9151 <cyjcode99@gmail.com>
2023-05-22 22:45:15 +0900

    🎯 Fix: color 변경
    1. color 변경

--- frontend/components/Player/PlaylistDrawer/style.tsx
+++ frontend/components/Player/PlaylistDrawer/style.tsx
@@ -61,6 +61,6 @@ export const ContentWrap = styled.div`
 export const OneMusicCard = styled.div<CardProps>`
   width: 100%;
   padding: 0.5rem;
-  background-color: ${(props) => props.active && "#dadade"};
+  background-color: ${(props) => props.active && "#7a7a7a"};
   border-radius: ${(props) => props.active && "10px"};
 `;
